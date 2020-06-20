6fa66274fe7be9c3531ee235fc0eb58f11fdd8f4
Honey <Honey@10.0.0.206>
2020-06-19 21:35:27 -0700

    testcommit2

--- src/default12/HELLOWORLD.java
+++ src/default12/HELLOWORLD.java
@@ -2,7 +2,7 @@ package default12;
 
 public class HELLOWORLD {
 	public static void main(String[] args) {
-		int x=10;
+		int x=10; int y=10;
 		System.out.println("hello");System.out.println("hello harshinianuroop");
 		System.out.println("hello harshinianuroopAmogha");
 		System.out.println("hello harshinianuroopAmogha" + x);
