c8ed201ef65ac6c38e6e617dbbdb8475ffd72064
anuroopharshini <30676383+anuroopharshini@users.noreply.github.com>
2020-06-19 21:10:00 -0700

    Update HELLOWORLD.java

--- src/default12/HELLOWORLD.java
+++ src/default12/HELLOWORLD.java
@@ -2,6 +2,7 @@ package default12;
 
 public class HELLOWORLD {
 	public static void main(String[] args) {
+		int x=10;
 		System.out.println("hello");System.out.println("hello harshinianuroop");
 		System.out.println("hello harshinianuroopAmogha");
 	}
